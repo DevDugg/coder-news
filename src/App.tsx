4a00cd55bd36c2f17a9781b877fd2240e2815786
VividTruthKeeper <mr.aydogdy02@gmail.com>
2024-09-05 16:46:14 +0500

    react router dom

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-import "./App.css";
-
-function App() {
-  return <h1>CODER NEWS</h1>;
-}
-
-export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import Home from "./pages/home.tsx";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
