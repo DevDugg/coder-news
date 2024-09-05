79fb65183d8c37807f8e0ad551425c5cdbce53a3
VividTruthKeeper <mr.aydogdy02@gmail.com>
2024-09-06 04:31:09 +0500

    route changed back to newstories

--- src/routes.ts
+++ src/routes.ts
@@ -7,7 +7,7 @@ const apiUrl: string = "https://hacker-news.firebaseio.com/v0";
 
 const routes: Route = {
   item: (itemId: string) => `/item/${itemId}.json`,
-  newStories: "/topstories.json",
+  newStories: "/newstories.json",
 };
 
 export { apiUrl, routes };
