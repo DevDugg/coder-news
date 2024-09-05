fb0420e7fe16f2668943cda4214a404515811f56
VividTruthKeeper <mr.aydogdy02@gmail.com>
2024-09-05 18:01:50 +0500

    browser router

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,14 @@
 import "./index.css";
 
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-
-import Home from "./pages/home.tsx";
+import App from "./app.tsx";
+import { BrowserRouter } from "react-router-dom";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-]);
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 );
