b059a2cf3920583e977571b084abb572ada58506
VividTruthKeeper <mr.aydogdy02@gmail.com>
2024-09-05 18:01:56 +0500

    home page created

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,12 @@
-function Home() {
-  return;
-}
+import LatestNews from "@/components/latest-news";
 
+const Home = () => {
+  return (
+    <main className="home">
+      <div className="container">
+        <LatestNews />
+      </div>
+    </main>
+  );
+};
 export default Home;
