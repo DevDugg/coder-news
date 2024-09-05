be0fdd6914f46c0c7059aa395e1a3606d3bd88fa
VividTruthKeeper <mr.aydogdy02@gmail.com>
2024-09-06 00:41:18 +0500

    refactored and added lazy loading options

--- src/hooks/use-fetch.ts
+++ src/hooks/use-fetch.ts
@@ -1,31 +1,48 @@
 import { useEffect, useState } from "react";
 
 import buildApiLink from "@/lib/build-api-link";
+import { useInView } from "react-intersection-observer";
 
-const useFetch = <Model>(path: string) => {
+interface UseFetchOptions {
+  lazy?: boolean;
+}
+
+const useFetch = <Model>(path: string, options: UseFetchOptions = {}) => {
+  const { lazy = false } = options;
   const [data, setData] = useState<Model | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<any>(null);
 
+  const { ref, inView } = useInView({
+    threshold: 0.1,
+    triggerOnce: true,
+    fallbackInView: !lazy,
+  });
+
   const url = buildApiLink(path);
 
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch(url);
+      const result = await response.json();
+      setData(result);
+    } catch (err) {
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(url);
-        const data = await response.json();
-        setData(data);
-      } catch (error) {
-        setError(error);
-      } finally {
-        setLoading(false);
-      }
-    };
+    if (!lazy) {
+      fetchData();
+    } else if (inView) {
+      fetchData();
+    }
+  }, [inView, url, lazy]);
 
-    fetchData();
-  }, [url]);
-
-  return { data, loading, error };
+  return { data, loading, error, ref: lazy ? ref : null };
 };
 
 export default useFetch;
