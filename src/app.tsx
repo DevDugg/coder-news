73b2e684352c6930ed8925132f3b5e1ebe00ce72
VividTruthKeeper <mr.aydogdy02@gmail.com>
2024-09-05 18:02:23 +0500

    routes

---
+++ src/app.tsx
@@ -0,0 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+
+import Header from "./components/header";
+import Home from "./pages/home";
+
+function App() {
+  return (
+    <div className="app font-inter bg-gray">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
