81966c01fac6ac18fde7eedccb3643498166bd0a
VividTruthKeeper <mr.aydogdy02@gmail.com>
2024-09-06 04:29:42 +0500

    Add useScrollToTop hook to scroll to top on page navigation

--- src/app.tsx
+++ src/app.tsx
@@ -3,8 +3,12 @@ import { Route, Routes } from "react-router-dom";
 import Header from "./components/header";
 import Home from "./pages/home";
 import Story from "./pages/story";
+import useScrollToTop from "./hooks/use-scroll-to-top";
 
 function App() {
+  // Function to scroll to top every time the user navigates to a new page
+  useScrollToTop();
+
   return (
     <div className="app font-inter bg-lightgray min-h-screen">
       <Header />
